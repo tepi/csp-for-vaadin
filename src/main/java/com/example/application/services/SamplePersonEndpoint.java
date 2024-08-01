41ede2aa69178c19befe91228ef2c7fcf28d6506
Teppo Kurki <teppo.kurki@vaadin.com>
2024-08-01 11:34:23 +0300

    Complex example CSP 24.5-SNAPSHOT

---
+++ src/main/java/com/example/application/services/SamplePersonEndpoint.java
@@ -0,0 +1,48 @@
+//package com.example.application.services;
+//
+//import java.util.Optional;
+//
+//import com.example.application.data.SamplePerson;
+//import org.springframework.dao.OptimisticLockingFailureException;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//
+//import com.vaadin.flow.server.auth.AnonymousAllowed;
+//import com.vaadin.hilla.Endpoint;
+//import com.vaadin.hilla.exception.EndpointException;
+//
+//@Endpoint
+//@AnonymousAllowed
+//public class SamplePersonEndpoint {
+//
+//    private final SamplePersonService service;
+//
+//    public SamplePersonEndpoint(SamplePersonService service) {
+//        this.service = service;
+//    }
+//
+//    public Page<SamplePerson> list(Pageable page) {
+//        return service.list(page);
+//    }
+//
+//    public Optional<SamplePerson> get(Long id) {
+//        return service.get(id);
+//    }
+//
+//    public SamplePerson update(SamplePerson entity) {
+//        try {
+//            return service.update(entity);
+//        } catch (OptimisticLockingFailureException e) {
+//            throw new EndpointException("Somebody else has updated the data while you were making changes.");
+//        }
+//    }
+//
+//    public void delete(Long id) {
+//        service.delete(id);
+//    }
+//
+//    public int count() {
+//        return service.count();
+//    }
+//
+//}
